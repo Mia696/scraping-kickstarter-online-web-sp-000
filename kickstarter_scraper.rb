73fd456a17e1f68ae283bee78fb85fba66426cac
Mia <tamiapaschall@gmail.com>
2020-02-12 23:27:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,6 @@
 # require libraries/modules here
 require "nokogiri"
+require "pry"
 
 
 def create_project_hash
@@ -7,6 +8,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
  
 kickstarter = Nokogiri::HTML(html)
+binding.pry
 end
 
-
+create_project_hash
